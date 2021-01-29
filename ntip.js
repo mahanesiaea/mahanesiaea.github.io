f69444b55e2515b31f1feeaf023b8cf418d9817f
mahanesiaea <66060963+mahanesiaea@users.noreply.github.com>
2021-01-29 15:13:41 +0100

    Add files via upload

---
+++ ntip.js
@@ -0,0 +1,94 @@
+ntipJSON='{"observation":[\
+{"property":"Melting Point","reference":"Joannis 1887","value":"369.65","units":"K"},\
+{"property":"Melting Point","reference":"Tammann 1889","value":"370.95","units":"K"},\
+{"property":"Melting Point","reference":"Bernini 1906","value":"370.75","units":"K"},\
+{"property":"Melting Point","reference":"Van Rossen 1912","value":"370.65","units":"K"},\
+{"property":"Melting Point","reference":"Rengade 1914","value":"371.0","units":"K"},\
+{"property":"Melting Point","reference":"Griffiths 1914","value":"370.76","units":"K"},\
+{"property":"Melting Point","reference":"Edmondson 1927","value":"370.85","units":"K"},\
+{"property":"Melting Point","reference":"Ladenburg-Thiele 1930","value":"370.95","units":"K"},\
+{"property":"Melting Point","reference":"Bridgman 1931 ","value":"370.77","units":"K"},\
+{"property":"Melting Point","reference":"Losana 1935","value":"370.85","units":"K"},\
+{"property":"Melting Point","reference":"Goria 1935","value":"370.85","units":"K"},\
+{"property":"Melting Point","reference":"Grube-Schmidt 1936","value":"371.15","units":"K"},\
+{"property":"Melting Point","reference":"Ginnings et al. 1950","value":"370.94","units":"K"},\
+{"property":"Melting Point","reference":"Douglas 1952","value":"370.97","units":"K"},\
+{"property":"Melting Point","reference":"Lyon-Miller  1954","value":"370.95","units":"K"},\
+{"property":"Melting Point","reference":"Douglas 1954","value":"370.97","units":"K"},\
+{"property":"Melting Point","reference":"Evans 1955","value":"370.97","units":"K"},\
+{"property":"Melting Point","reference":"Kothari 1956","value":"370.5","units":"K"},\
+{"property":"Melting Point","reference":"Bradshaw-Pearson 1956","value":"370.96","units":"K"},\
+{"property":"Melting Point","reference":"Suttle 1957","value":"370.65","units":"K"},\
+{"property":"Melting Point","reference":"Kutateladze 1959","value":"371.0","units":"K"},\
+{"property":"Melting Point","reference":"Grosse 1961","value":"371","units":"K"},\
+{"property":"Melting Point","reference":"Ponyatovskiy 1961","value":"370.65","units":"K"},\
+{"property":"Melting Point","reference":"Newton 1962","value":"371.15","units":"K"},\
+{"property":"Melting Point","reference":"Hultgren 1963","value":"370.98","units":"K"},\
+{"property":"Melting Point","reference":"Kraut-Kennedy 1966","value":"370.9","units":"K"},\
+{"property":"Melting Point","reference":"Martin 1967","value":"371.01","units":"K"},\
+{"property":"Melting Point","reference":"Luedemann-Kennedy 1968","value":"371.15","units":"K"},\
+{"property":"Melting Point","reference":"Ott-Goates 1968","value":"370.98","units":"K"},\
+{"property":"Melting Point","reference":"Ott-Goates 1971","value":"371.05","units":"K"},\
+{"property":"Melting Point","reference":"Schmid-Fritsch 1969","value":"370.923","units":"K"},\
+{"property":"Melting Point","reference":"Ivanov 1970","value":"370.8","units":"K"},\
+{"property":"Melting Point","reference":"Shpilrain 1970","value":"371","units":"K"},\
+{"property":"Melting Point","reference":"Yunker 1970","value":"371.0","units":"K"},\
+{"property":"Melting Point","reference":"Anderson 1970","value":"371.05","units":"K"},\
+{"property":"Melting Point","reference":"Foust 1972","value":"370.97","units":"K"},\
+{"property":"Melting Point","reference":"Ho-Powell-Liley 1972","value":"371","units":"K"},\
+{"property":"Melting Point","reference":"Faber 1972","value":"371","units":"K"},\
+{"property":"Melting Point","reference":"Stroud-Ashcroft 1972","value":"390","units":"K"},\
+{"property":"Melting Point","reference":"Fritsch 1973","value":"371","units":"K"},\
+{"property":"Melting Point","reference":"Hultgren 1973","value":"371","units":"K"},\
+{"property":"Melting Point","reference":"Makarenko 1974","value":"370.98","units":"K"},\
+{"property":"Melting Point","reference":"Adlhart-Fritsch 1974","value":"370.98","units":"K"},\
+{"property":"Melting Point","reference":"Ivanov 1974","value":"371.26","units":"K"},\
+{"property":"Melting Point","reference":"Vargaftik 1975","value":"371","units":"K"},\
+{"property":"Melting Point","reference":"Mirwald-Kennedy 1975","value":"370.21","units":"K"},\
+{"property":"Melting Point","reference":"Kubaschewski-Alcock 1979","value":"371.05","units":"K"},\
+{"property":"Melting Point","reference":"Thurnay 1981","value":"371","units":"K"},\
+{"property":"Melting Point","reference":"Gurvich 1982","value":"371.01","units":"K"},\
+{"property":"Melting Point","reference":"Kamioka 1983","value":"370.95","units":"K"},\
+{"property":"Melting Point","reference":"Boehler 1983","value":"371","units":"K"},\
+{"property":"Melting Point","reference":"Pelissier 1983","value":"361","units":"K"},\
+{"property":"Melting Point","reference":"Ohse 1985","value":"370.9","units":"K"},\
+{"property":"Melting Point","reference":"Zha 1985","value":"371","units":"K"},\
+{"property":"Melting Point","reference":"JANAF-Chase 1985","value":"370.98","units":"K"},\
+{"property":"Melting Point","reference":"Iida -Guthrie 1988","value":"371.05","units":"K"},\
+{"property":"Melting Point","reference":"Bystrov 1990","value":"371.02","units":"K"},\
+{"property":"Melting Point","reference":"Alcock 1994","value":"370.95","units":"K"},\
+{"property":"Melting Point","reference":"Dass 1995","value":"370.95","units":"K"},\
+{"property":"Melting Point","reference":"Cheynet 1996","value":"371","units":"K"},\
+{"property":"Melting Point","reference":"Morita 1998","value":"370.98","units":"K"},\
+{"property":"Melting Point","reference":"IAEA 2002","value":"371.15","units":"K"},\
+{"property":"Melting Point","reference":"Gale 2003","value":"370.95","units":"K"},\
+{"property":"Melting Point","reference":"Gregoryanz 2005","value":"370.9","units":"K"},\
+{"property":"Melting Point","reference":"IAEA 2008","value":"371","units":"K"},\
+{"property":"Melting Point","reference":"Cacuci 2010","value":"370.8","units":"K"},\
+{"property":"Melting Point","reference":"Sobolev 2011","value":"371","units":"K"},\
+{"property":"Melting Point","reference":"Rousseau 2011","value":"370.8","units":"K"},\
+{"property":"Melting Point","reference":"IAEA 2012","value":"371","units":"K"},\
+{"property":"Melting Point","reference":"Arafin 2014","value":"417.6","units":"K"},\
+{"property":"Melting Point","reference":"Hieu 2015","value":"371","units":"K"},\
+{"property":"Boiling Point","reference":"Fink & Leibowitz 1995","value":"1154.7","units":"K"},\
+{"property":"Boiling Point","reference":"R. Hultgren 1963","value":"1156.2","units":"K"},\
+{"property":"Boiling Point","reference":"O. Kubaschewski and C. Alcock 1979","value":"1154.7","units":"K"},\
+{"property":"Boiling Point","reference":"E. Kazenas and G. Astakhova 1997","value":"1166","units":"K"},\
+{"property":"Boiling Point","reference":"C. Ewing et al. 1966","value":"1154.6","units":"K"},\
+{"property":"Boiling Point","reference":"A. Mozgovoi et al. 1987","value":"1156.3","units":"K"},\
+{"property":"Boiling Point","reference":"D. Cacuci et al. 2010","value":"1155.8","units":"K"},\
+{"property":"Boiling Point","reference":"K. Morita, E. Fischer 1988","value":"1156.5","units":"K"},\
+{"property":"Boiling Point","reference":"C. Lee, D. Lee and C. Bonilla 1969","value":"1154.6","units":"K"},\
+{"property":"Critical Temperature","reference":"Achener 1968","value":"2573","units":"K"},\
+{"property":"Critical Temperature","reference":"Vargaftik 1975","value":"2500","units":"K"},\
+{"property":"Critical Temperature","reference":"Bhise-Bonilla 1976","value":"2503.3","units":"K"},\
+{"property":"Critical Temperature","reference":"Das Gupta 1977","value":"2508.7","units":"K"},\
+{"property":"Critical Temperature","reference":"Fink-Leibowitz 1979","value":"2509.46","units":"K"},\
+{"property":"Critical Temperature","reference":"Thurnay 1982","value":"2508","units":"K"},\
+{"property":"Critical Temperature","reference":"Binder 1984","value":"2485","units":"K"},\
+{"property":"Critical Temperature","reference":"Petiot-Seiler 1984","value":"2630","units":"K"},\
+{"property":"Critical Temperature","reference":"Ohse 1985","value":"2497","units":"K"},\
+{"property":"Critical Temperature","reference":"Shpilrain 1985","value":"2505","units":"K"},\
+{"property":"Critical Temperature","reference":"Browning-Potter 1985","value":"2503.7","units":"K"},\
+{"property":"Critical Temperature","reference":"Bystrov 1990","value":"2503","units":"K"}\
+]}';
